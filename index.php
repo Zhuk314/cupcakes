c436654855127d215bbaa630c3c6dba9887a073b
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-05-14 01:03:00 -0400

    Clean up the validation and controler from unused elements, create appropriate displaying of summary flavors

--- index.php
+++ index.php
@@ -36,18 +36,23 @@ $f3->route('GET|POST /', function($f3){
             $f3->set('errors["name"]', 'Invalid name');
         }
 
-        if (!empty($_POST['cups'])) {
+        // Valid cupcake flavors
+        $userCups = $_POST['cups'];
 
-            $userCups = $_POST['cups'];
+        if (validFlavor($userCups)) {
+            //$_SESSION['cups'] = $userCups;
+            $cupcakes = getFlavors();
+            $_SESSION['cups'] = array();
 
-            //Validate Cupcakes
-            if (validFlavor($userCups)) {
-                $_SESSION['cups'] = $userCups;
-            } else {
-                $f3->set('errors["cups"]', 'Invalid flavors');
+            foreach ($userCups as $cup){
+                array_push($_SESSION['cups'], $cupcakes[$cup]);
+                var_dump($_SESSION['cups']);
             }
+        } else {
+            $f3->set('errors["cups"]', 'Invalid name');
         }
 
+        // If no errors, display the summary page
         if (empty($f3->get('errors'))) {
             header('location: summary');
         }
@@ -58,14 +63,13 @@ $f3->route('GET|POST /', function($f3){
     echo $view->render('views/home.html');
 });
 
-$f3->route('GET|POST /home', function($f3){
+// Define the route for summary page
+$f3->route('GET|POST /summary', function($f3){
 
-    //Get the data from the model
-    $f3->set('cupcakes', getFlavors());
 
-    //Display the home page
+    //Display the summary page
     $view = new Template();
-    echo $view->render('views/home.html');
+    echo $view->render('views/summary.html');
 });
 
 //Run Fat-Free
--- model/validation.php
+++ model/validation.php
@@ -1,11 +1,11 @@
 <?php
-//Turn on error-reporting
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
 /*validation.php
  * Validate data for the cupcakes
  */
+
+//Turn on error-reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 function validName($name)
 {
@@ -18,29 +18,14 @@ function validFlavor($flavors)
 
     // Check does the variable is array at first, if not, return false immediately
     if(!is_array($flavors)){
-        echo "<p>$flavors not array.<p>";
         return false;
     }
 
     //Make sure each selected flavor is valid
     foreach ($flavors as $userChoice) {
         if (!in_array($userChoice, $validFlavors) && count($flavors)>0) {
-            echo "<p>Hey, actually $userChoice WORK.<p>";
             return true;
         }
-        echo "<p>Hey, actually $userChoice doesnt WORK.<p>";
         return false;
     }
-    /*
-    $validFlavors = getFlavors();
-
-
-    foreach ($flavors as $userChoice => $userChoice_value) {
-        if (!in_array($userChoice, $validFlavors)) {
-            echo "<p>Hey, actually $userChoice FUCKING WORK.<p>";
-            return false;
-        }
-    }
-    return true;
-    */
 }
